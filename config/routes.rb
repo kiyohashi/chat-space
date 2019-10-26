c5bd04069c2c74e7ed22467f40b476cf750dd5bd
kiyohashi <iz.kk.r77@gmail.com>
2019-10-26 16:10:25 +0900

    Implementationgroupfunction

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-<<<<<<< Updated upstream
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-=======
 
   devise_for :users
   root to: 'groups#index'
@@ -9,5 +6,4 @@ Rails.application.routes.draw do
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index]
   end
->>>>>>> Stashed changes
 end
